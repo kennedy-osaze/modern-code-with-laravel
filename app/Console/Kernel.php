63c000ebc2f8ed6e3d2f7c10c920b318ec0e71db
Kennedy Osaze <me.osaze@gmail.com>
2020-01-11 14:36:49 +0100

    Add PHP Mess Detector

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -22,11 +21,11 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')
-        //          ->hourly();
-    }
+    // protected function schedule(Schedule $schedule)
+    // {
+    //     // $schedule->command('inspire')
+    //     //          ->hourly();
+    // }
 
     /**
      * Register the commands for the application.
